ca70ea790e11a00e55768b0d8075357f8350bb4b
BlackCyber21 <rafael_for.work@hotmail.com>
2021-03-31 22:01:39 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+desenvolvedores = [
+    {
+        'id':'0',
+        'nome':'Rafael',
+        'habilidades':['Python', 'Flask']
+    },
+
+    {
+        'id':'1',
+        'nome':'Costa',
+        'habilidades':['Sql', 'Html']}
+]
+
+class Desenvolvedor(Resource):
+    def get(self, id):
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            mensagem = 'Desenvolvedor de ID {} não existe'.format(id)
+            response = {'status': 'erro', 'mensagem': mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procute o administrador da API'
+            response = {'status': 'erro', 'mensagem': mensagem}
+        return (response)
+
+    def put(self, id):
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return (dados)
+
+    def delete(self, id):
+        desenvolvedores.pop(id)
+        return {'status': 'sucesso', 'mensagem': 'Registro excluído'}
+
+# Lista de todos os desenvolvedores e permite registrar um novo desenvolvedor
+class ListaDesenvolvedores(Resource):
+    def get(self):
+        return desenvolvedores
+
+    def post(self):
+        dados = json.loads(request.data)
+        posicao = len(desenvolvedores)
+        dados['id'] = posicao
+        desenvolvedores.append(dados)
+        return desenvolvedores[posicao]
+
+api.add_resource(Desenvolvedor, '/dev/<int:id>')
+api.add_resource(ListaDesenvolvedores, '/dev/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
